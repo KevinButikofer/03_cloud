e4c3ec5e9b8b87dc74321077e40c8f05c4e05aa8
lewisjaggi <lewisjaggi@gmail.com>
2019-11-04 17:24:16 +0100

    cloud 03

--- restserver-with-swift-S3.py
+++ restserver-with-swift-S3.py
@@ -123,9 +123,9 @@ def routeStoreBetweenDateSwift(controller, sensor_id, from_date, to_date):
 
 def storeDataSwift(object_name, object_data):
     conn = create_connection_switch(SWITCH_AUTH_URL,openstack_tenant, openstack_user, openstack_pwd, SWITCH_REGION)
-    conn.create_container(CONTAINER_NAME)
-    conn.create_object(container=CONTAINER_NAME, name =object_name, data = object_data )
-    return SWITCH_CONTAINER_URL % (CONTAINER_NAME, object_name)
+    container = conn.create_container(CONTAINER_NAME)
+    conn.create_object(container=container.name, name =object_name, data = object_data )
+    return f"{SWITCH_CONTAINER_URL}, {container.name}, {object_name})"
 
 def storeDataS3(object_name, object_data):
     #TODO
